dad22e66e62c911fe36cd3d4975973bb77ea4e45
Jesus <jflorespadilla@nevada.unr.edu>
2019-03-25 22:53:43 -0700

    Added manager interface and derived classes

--- UA4/Assets/scripts/CollectibleItem.cs
+++ UA4/Assets/scripts/CollectibleItem.cs
@@ -5,7 +5,7 @@ public class CollectibleItem : MonoBehaviour {
     [SerializeField] private string itemName;
 
     private void OnTriggerEnter(Collider other) {
-        Debug.Log("Item collected: " + itemName);
+        Managers.Inventory.AddItem(itemName);
         Destroy(this.gameObject);
     }
 }
